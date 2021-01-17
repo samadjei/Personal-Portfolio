37ae80ecf15a5a0130b998539ff09097a667c5cd
Samuel Adjei <info.samadjei@gmail.com>
2021-01-17 23:21:54 +0000

    added blog templates

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+// create pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      blogs: allStrapiBlogs {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
+
+  // everytime we click on a blog, it programatically runs the page
+  result.data.blogs.nodes.forEach(blog => {
+    createPage({
+      path: `/blogs/${blog.slug}`,
+      component: path.resolve(`src/templates/blog-template.js`),
+      context: {
+        slug: blog.slug,
+      },
+    })
+  })
+}
