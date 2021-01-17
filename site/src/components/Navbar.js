c2a2eb34596161cb1e13656d9f5c7f04bfc4b8ca
Samuel Adjei <info.samadjei@gmail.com>
2021-01-17 17:06:22 +0000

    switched navigation to a class-based component

--- site/src/components/Navbar.js
+++ site/src/components/Navbar.js
@@ -1,92 +1,79 @@
-import React, { useState } from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import NavSocial from "../constants/sociallinks"
 
-const Navbar = () => {
-  const [click, setClick] = useState(false)
-  const [navbar, setNavbar] = useState(false)
+class Navbar extends Component {
+  state = { clicked: false }
 
-  const handleClick = () => setClick(!click)
-  const closeMobileMenu = () => setClick(false)
+  handleClick = () => {
+    // set click to the opposite value whenever you click it
+    this.setState({ clicked: !this.state.clicked })
+  }
 
-  return (
-    <>
-      <nav className={navbar ? "navbar active" : "navbar"}>
-        <div className="navbar__container">
-          <div className="navbar--logo">
-            <Link to="/">
-              <h3>Samuel Adjei</h3>
-            </Link>
-          </div>
-          <div className="navbar__links">
-            <button
-              onClick={handleClick}
-              className={`hamburger menu-icon hamburger--collapse-r ${
-                click ? "is-active" : "hamburger"
-              }`}
-              type="button"
-            >
-              <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-              </span>
-            </button>
-
-            <div className="navbar__menus">
-              <ul className={click ? "menu active" : "menu"}>
-                <li className="menu__item">
-                  <Link
-                    to="/work"
-                    className="menu--links"
-                    onClick={closeMobileMenu}
-                  >
-                    Work
-                  </Link>
-                </li>
-                <li className="menu__item">
-                  <Link
-                    to="/about"
-                    className="menu--links"
-                    onClick={closeMobileMenu}
-                  >
-                    About
-                  </Link>
-                </li>
-                <li className="menu__item">
-                  <Link
-                    to="/blog"
-                    className="menu--links"
-                    onClick={closeMobileMenu}
-                  >
-                    Blog
-                  </Link>
-                </li>
-                <li className="menu__item">
-                  <a
-                    className="menu--links-mobile"
-                    href="mailto:info.samadjei@gmail.com"
-                    onClick={closeMobileMenu}
-                  >
-                    Contact
-                  </a>
-                </li>
-                <NavSocial />
-              </ul>
+  render() {
+    return (
+      <>
+        <nav className={this.state.clicked ? "navbar active" : "navbar"}>
+          <div className="navbar__container">
+            <div className="navbar--logo">
+              <Link to="/">
+                <h3>Samuel Adjei</h3>
+              </Link>
             </div>
-            <div id="cta">
-              <a
-                className="btn btn-nav"
-                buttonStyle="btn--outline"
-                buttonSize="btn--desktop"
-                href="hire"
+            <div className="navbar__links">
+              <button
+                onClick={this.handleClick}
+                className={`hamburger menu-icon hamburger--collapse-r ${
+                  this.state.clicked ? "is-active" : "hamburger"
+                }`}
+                type="button"
               >
-                Hire Me
-              </a>
+                <span class="hamburger-box">
+                  <span class="hamburger-inner"></span>
+                </span>
+              </button>
+
+              <div className="navbar__menus">
+                <ul className={this.state.clicked ? "menu active" : "menu"}>
+                  <li className="menu__item">
+                    <Link to="/work" className="menu--links">
+                      Work
+                    </Link>
+                  </li>
+                  <li className="menu__item">
+                    <Link to="/about" className="menu--links">
+                      About
+                    </Link>
+                  </li>
+                  <li className="menu__item">
+                    <Link to="/blog" className="menu--links">
+                      Blog
+                    </Link>
+                  </li>
+                  <li className="menu__item">
+                    <a className="menu--links-mobile" href="#hire">
+                      Contact
+                    </a>
+                  </li>
+                  <NavSocial />
+                </ul>
+              </div>
+              <div id="cta">
+                <a
+                  className="btn btn-nav"
+                  buttonStyle="btn--outline"
+                  buttonSize="btn--desktop"
+                  href="#hire"
+                >
+                  Hire Me
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </>
-  )
+        </nav>
+      </>
+    )
+  }
 }
 
 export default Navbar
--- site/src/pages/About.js
+++ site/src/pages/About.js
@@ -11,73 +11,74 @@ const About = () => {
         <SEO title="About" />
         <Title className="projects--title" title="About Samuel Adjei"></Title>
         <div class="about">
-          <div className="about__content ">
-            <p className="about__content--text">
-              Hey there! I'm Samuel, a freelance web developer from Slough, UK.
-              I enjoy creating websites and web applications the utilise the
-              latest technology available. I have over 2 years experience as a
-              self-taught web developer and i am continuosly striving to improve
-              my knowldge and skillset to provide the most value for my clients.
-            </p>
-            <p className="about__content--text">
-              I am enrolled in a 4 year course at the{" "}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-                href="https://www.uwe.ac.uk/"
-              >
-                University of the West of England
-              </a>{" "}
-              studying{" "}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-                href="https://www.tpdegrees.com/degrees/courses-placements-jobs/universities-list-itmb/"
-              >
-                ITMB
-              </a>
-              .Currently, i am doing a placement at{" "}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-                href="https://www.pernod-ricard.com/en"
-              >
-                Pernod Ricard
-              </a>{" "}
-              as an IT Solution Intern.
-            </p>
-            <div className="stack">
-              <h4>Here's a few technologies i work with:</h4>
-              <div className="stack__grid">
-                <div className="stack__content">
-                  <h5 className="stack--title">Front-end</h5>
-                  <ul>
-                    <li className="stack--items">HTML & (S)CSS</li>
-                    <li className="stack--items">JavaScript</li>
-                    <li className="stack--items">React</li>
-                    <li className="stack--items">WordPress</li>
-                  </ul>
-                </div>
-                <div className="stack__content">
-                  <h5>Back-end</h5>
-                  <ul>
-                    <li className="stack--items">PHP</li>
-                  </ul>
-                </div>
-                <div className="stack__content">
-                  <h5>Design</h5>
-                  <ul>
-                    <li className="stack--items">Adobe XD</li>
-                    <li className="stack--items">Figma</li>
-                  </ul>
-                </div>
-              </div>
+               <div className="about__content ">
+                  <p className="about__content--text">
+                     Hey there! I'm Samuel, a freelance web developer from
+                     Slough, UK. I enjoy creating websites and web applications
+                     the utilise the latest technology available. I have over 2
+                     years experience as a self-taught web developer and i am
+                     continuosly striving to improve my knowldge and skillset to
+                     provide the most value for my clients.
+                  </p>
+                  <p className="about__content--text">
+                     I am enrolled in a 4 year course at the{" "}
+                     <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="underline"
+                        href="https://www.uwe.ac.uk/"
+                     >
+                        University of the West of England
+                     </a>{" "}
+                     studying{" "}
+                     <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="underline"
+                        href="https://www.tpdegrees.com/degrees/courses-placements-jobs/universities-list-itmb/"
+                     >
+                        ITMB
+                     </a>
+                     .Currently, i am doing a placement at{" "}
+                     <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="underline"
+                        href="https://www.pernod-ricard.com/en"
+                     >
+                        Pernod Ricard
+                     </a>{" "}
+                     as an IT Solution Intern.
+                  </p>
+                  <div className="stack">
+                     <h4>Here's a few technologies i work with:</h4>
+                     <div className="stack__grid">
+                        <div className="stack__content">
+                           <h5 className="stack--title">Front-end</h5>
+                           <ul>
+                              <li className="stack--items">HTML & (S)CSS</li>
+                              <li className="stack--items">JavaScript</li>
+                              <li className="stack--items">React</li>
+                              <li className="stack--items">WordPress</li>
+                           </ul>
+                        </div>
+                        <div className="stack__content">
+                           <h5>Back-end</h5>
+                           <ul>
+                              <li className="stack--items">PHP</li>
+                           </ul>
+                        </div>
+                        <div className="stack__content">
+                           <h5>Design</h5>
+                           <ul>
+                              <li className="stack--items">Adobe XD</li>
+                              <li className="stack--items">Figma</li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-          </div>
-        </div>
       </div>
     </Layout>
   )
